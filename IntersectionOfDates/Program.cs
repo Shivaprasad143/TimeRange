392f6f6ca40102865b9b03b618e628c4654d562f
CORP\Shiva.Marimallaiah <shivaprasadcool06@gmail.com>
2017-01-28 11:12:29 +0530

    Completed

---
+++ IntersectionOfDates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersectionOfDates
+{
+   class TimeRange
+        {
+            public DateTime StartDate { get; set; }
+
+            public DateTime EndDate { get; set; }
+
+            public TimeRange(DateTime start, DateTime end)
+            {
+                this.StartDate = start;
+                this.EndDate = end;
+            }
+
+            public TimeRange()
+            {
+
+            }
+        }
+
+
+    }
+
+
